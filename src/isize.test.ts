b1fc9f6b5f78f801a2b3c330aaeb8d6558341ea8
rarecoil <rarecoil@users.noreply.github.com>
2019-07-11 13:46:21 -0700

    try to avoid shell:true

--- src/isize.test.ts
+++ src/isize.test.ts
@@ -4,7 +4,7 @@ import { resolve, join } from 'path';
 
 // Get ISIZE from `tar` in order to check our own calculation
 const getCompressionInfoFromGzip = (filePath:string):Array<number> => {
-    let result = spawnSync('/usr/bin/gzip', ['-l', filePath], { stdio: 'pipe' });
+    let result = spawnSync('/usr/bin/env', ['gzip', '-l', filePath], { stdio: 'pipe' });
     let stdout = result.stdout.toString().split("\n");
     let re = new RegExp("\\s+(?<comp>\\d+)\\s+(?<uncomp>\\d+)");
     if (stdout.length === 3) {
