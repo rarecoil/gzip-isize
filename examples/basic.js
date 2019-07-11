85b2d7e156109a25cfb49af8a58aef22e6f97f49
rarecoil <rarecoil@users.noreply.github.com>
2019-07-11 13:32:38 -0700

    set indentation in example

--- examples/basic.js
+++ examples/basic.js
@@ -1,6 +1,6 @@
 const isz = require('../dist/isize.js').ISize;
 
 isz.get('../test/file.tgz')
-   .then((result) => {
-      console.log(result);
+    .then((result) => {
+        console.log(result);
     });
